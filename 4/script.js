16dc01fe52ff3966d3b16d93b74e78766f940359
Fraser Xu <xvfeng123@gmail.com>
2020-12-05 11:28:11 +1100

    4

--- 4/script.js
+++ 4/script.js
@@ -1,40 +1,71 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs")
+const path = require("path")
 
 const fsPromises = fs.promises
 
 const fields = {
-  byr: 'required',
-  iyr: 'required',
-  eyr: 'required',
-  hgt: 'required',
-  hcl: 'required',
-  ecl: 'required',
-  pid: 'required',
-  cid: 'optional',
+  byr: (value) => {
+    if (!value) return false
+    return /^\d{4}$/.test(value) && value >= 1920 && value <= 2002
+  },
+  iyr: (value) => {
+    if (!value) return false
+    return /^\d{4}$/.test(value) && value >= 2010 && value <= 2020
+  },
+  eyr: (value) => {
+    if (!value) return false
+    return /^\d{4}$/.test(value) && value >= 2020 && value <= 2030
+  },
+  hgt: (value) => {
+    if (!value) return false
+    return /^(15[0-9]|1[6-8][0-9]|19[0-3])cm|^(59|6[0-9]|7[0-6])in$/.test(value)
+  },
+  hcl: (value) => {
+    if (!value) return false
+    return /^#[0-9a-f]{6}$/.test(value)
+  },
+  ecl: (value) => {
+    if (!value) return false
+    const validOptions = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    return validOptions.includes(value)
+  },
+  pid: (value) => {
+    if (!value) return false
+    return /^\d{9}$/.test(value)
+  },
+  cid: () => {
+    return true
+  },
 }
 
-const parseEntry = str => {
-  return str.replace(/\n/g, ' ').split(' ').reduce((acc, current) => {
-    const [key, value] = current.split(':')
-    acc[key] = value
-    return acc
-  }, {})
+const parseEntry = (str) => {
+  return str
+    .replace(/\n/g, " ")
+    .split(" ")
+    .reduce((acc, current) => {
+      const [key, value] = current.split(":")
+      acc[key] = value
+      return acc
+    }, {})
 }
 
-const inValidEntry = entry => {
-  return Object.keys(fields).some(key => {
-    return fields[key] === 'required' && !entry[key]
+const validateEntry = (entry) => {
+  return Object.keys(fields).every((key) => {
+    return fields[key](entry[key])
   })
 }
 
 const main = async () => {
   try {
-    const input = await fsPromises.readFile(path.resolve(__dirname, './input.txt'), 'utf8')
-    const inValidEntries = input.trim().split('\n\n')
-      .map(parseEntry)
-      .filter(inValidEntry)
-    console.log('inValidEntries:', inValidEntries.length)
+    const input = await fsPromises.readFile(
+      path.resolve(__dirname, "./input.txt"),
+      "utf8"
+    )
+    const allEntries = input.trim().split("\n\n").map(parseEntry)
+
+    const validEntries = allEntries.filter(validateEntry)
+    console.log("all entries", allEntries.length)
+    console.log("valid entries:", validEntries.length)
   } catch (error) {
     console.error(error)
   }
